552b47fcd95985faa5521d7c375c28f3671a9b82
jaz801 <34124216+jaz801@users.noreply.github.com>
2023-08-07 19:53:16 +0000

    selenium working YEAH

--- selenium_script.py
+++ selenium_script.py
@@ -3,10 +3,18 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
+url = 'https://www.youtube.com/watch?v=FcW-AXsirBE&ab_channel=Jovian'
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https:/google.com")
+def get_driver():
+ chrome_options = Options()
+ chrome_options.add_argument('--no-sandbox')
+ chrome_options.add_argument('--headless')
+ chrome_options.add_argument('--disable-dev-shm-usage')
+ driver = webdriver.Chrome(options=chrome_options)
+ return driver
 
+if __name__== "__main__":
+  driver = get_driver()
+  driver.get(url)
+  print('page title',driver.title)
+ 
